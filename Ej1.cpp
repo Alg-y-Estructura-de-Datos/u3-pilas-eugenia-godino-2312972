ccf2202c0b60d52fb4019f5e629d1cc1a6f560a2
euge7777 <2312972@ucc.edu.ar>
2024-09-03 11:09:33 -0300

    completado

--- Ej1.cpp
+++ Ej1.cpp
@@ -1,9 +1,21 @@
 #include <iostream>
+#include "Pila/Pila.h"
 using namespace std;
 
 int main() {
+    Pila<char> pila;
+    char letra;
+    cout << "Imgrese una palabra o frase (finalice con un punto)" << endl;
+    do {
+        cin.get(letra);
+        if (letra != '.'){
+            pila.push(letra);
+        }
+    } while (letra != '.');
 
-    cout << "Ejercicio N° 1" << endl;
-
+    cout << "La palabra o frase invertida queda: " << endl; 
+    while (!pila.esVacia()){
+        cout << pila.pop();
+    }
     return 0;
 }
